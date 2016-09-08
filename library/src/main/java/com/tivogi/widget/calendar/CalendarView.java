368ab0af43bb63940e8311717dcef3d498575ac6
Igor Gozhenko <igor.gozhenko@gmail.com>
2016-09-08 23:49:21 +0300

    Add method to control using of progress during update cells

--- library/src/main/java/com/tivogi/widget/calendar/CalendarView.java
+++ library/src/main/java/com/tivogi/widget/calendar/CalendarView.java
@@ -24,229 +24,233 @@ import com.tivogi.calendar_view.R;
 
 public abstract class CalendarView<T extends DaysGridView> extends FrameLayout implements OnDateSetListener {
 
-	private final class OnClickListenerImplementation implements OnClickListener {
-		@Override
-		public void onClick(View v) {
-			int id = v.getId();
-			if (id == R.id.vc_button_next_month) {
-				addMonth(1);
-			} else if (id == R.id.vc_button_previous_month) {
-				addMonth(-1);
-			} else if (id == R.id.vc_button_next_year) {
-				addMonth(12);
-			} else if (id == R.id.vc_button_previous_year) {
-				addMonth(-12);
-			} else if (id == R.id.vc_button_pick_date) {
-				new DatePickerDialog(getContext(), CalendarView.this, mDateTime.getYear(), mDateTime.getMonth() - 1, mDateTime.getDay()).show();
-			}
-		}
-	}
+    private TextView mTitleView;
+    private DaysGridView mCurrentDaysGridView;
+    private OnClickListener mOnClickListener;
+    private LinearLayout mWeekHeaderLinearLayout;
+    private TextView[] mWeekHeaderTextViews;
+    private OnDateClickListener mOnDateClickListener;
+    private ViewSwitcher mSwitcherView;
+    private boolean mSelectable = true;
+    private DateTime mDateTime;
+    private OnSwipeTouchListener mOnSwipeTouchListener = new OnSwipeTouchListener(getContext()) {
+        @Override
+        public boolean onSwipeLeft() {
+            addMonth(1);
+            return true;
+        }
 
-	public interface OnDateClickListener {
-		void onDateClick(DateTime date);
-	}
+        @Override
+        public boolean onSwipeRight() {
+            addMonth(-1);
+            return true;
+        }
+    };
+    private DateTime mSelectedDate;
+    private View mProgressView;
+    private boolean showProgress = false;
 
-	private TextView mTitleView;
-	private DaysGridView mCurrentDaysGridView;
-	private OnClickListener mOnClickListener;
-	private LinearLayout mWeekHeaderLinearLayout;
-	private TextView[] mWeekHeaderTextViews;
-	private OnDateClickListener mOnDateClickListener;
-	private ViewSwitcher mSwitcherView;
-	private boolean mSelectable = true;
-	private OnSwipeTouchListener mOnSwipeTouchListener = new OnSwipeTouchListener(getContext()) {
-		@Override
-		public boolean onSwipeLeft() {
-			addMonth(1);
-			return true;
-		}
+    public CalendarView(Context context) {
+        super(context);
+        initialize(context);
+    }
 
-		@Override
-		public boolean onSwipeRight() {
-			addMonth(-1);
-			return true;
-		}
-	};
-	private DateTime mDateTime;
-	private DateTime mSelectedDate;
+    public CalendarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context);
+    }
 
-	private View mProgressView;
+    protected void addMonth(int countOfMonth) {
+        if (countOfMonth > 0) {
+            setDate(mDateTime.plus(0, countOfMonth, 0, 0, 0, 0, 0, DayOverflow.LastDay));
+        } else {
+            setDate(mDateTime.minus(0, -countOfMonth, 0, 0, 0, 0, 0, DayOverflow.LastDay));
+        }
+    }
 
-	public CalendarView(Context context) {
-		super(context);
-		initialize(context);
-	}
+    private void bindViews() {
+        mSwitcherView = (ViewSwitcher) findViewById(R.id.vc_switcher);
+        mProgressView = findViewById(R.id.vc_progress_wrapper);
+        for (int i = 0; i < 2; i++) {
+            T daysGridView = createDaysGridView();
+            daysGridView.setBackgroundColor(getResources().getColor(R.color.days_grid_background));
+            daysGridView.setCalendarView(this);
+            mSwitcherView.addView(daysGridView);
+        }
+        mCurrentDaysGridView = (DaysGridView) mSwitcherView.getCurrentView();
+        mTitleView = (TextView) findViewById(R.id.vc_button_pick_date);
+        mWeekHeaderLinearLayout = (LinearLayout) findViewById(R.id.vc_week_header);
+    }
 
-	public CalendarView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		initialize(context);
-	}
+    protected abstract T createDaysGridView();
 
-	protected void addMonth(int countOfMonth) {
-		if (countOfMonth > 0) {
-			setDate(mDateTime.plus(0, countOfMonth, 0, 0, 0, 0, 0, DayOverflow.LastDay));
-		} else {
-			setDate(mDateTime.minus(0, -countOfMonth, 0, 0, 0, 0, 0, DayOverflow.LastDay));
-		}
-	}
+    private TextView createWeekHeaderTextView() {
+        TextView result = new TextView(getContext());
+        result.setTextSize(14f); // TODO configurable via xml
+        result.setTextColor(Color.WHITE); // TODO configurable via xml
+        result.setGravity(Gravity.CENTER);
+        return result;
+    }
 
-	private void bindViews() {
-		mSwitcherView = (ViewSwitcher) findViewById(R.id.vc_switcher);
-		mProgressView = findViewById(R.id.vc_progress_wrapper);
-		for (int i = 0; i < 2; i++) {
-			T daysGridView = createDaysGridView();
-			daysGridView.setBackgroundColor(getResources().getColor(R.color.days_grid_background));
-			daysGridView.setCalendarView(this);
-			mSwitcherView.addView(daysGridView);
-		}
-		mCurrentDaysGridView = (DaysGridView) mSwitcherView.getCurrentView();
-		mTitleView = (TextView) findViewById(R.id.vc_button_pick_date);
-		mWeekHeaderLinearLayout = (LinearLayout) findViewById(R.id.vc_week_header);
-	}
+    protected DaysGridView getCurrentDaysGridView() {
+        return mCurrentDaysGridView;
+    }
 
-	protected abstract T createDaysGridView();
+    public DateTime getDate() {
+        return mDateTime;
+    }
 
-	private TextView createWeekHeaderTextView() {
-		TextView result = new TextView(getContext());
-		result.setTextSize(14f); // TODO configurable via xml
-		result.setTextColor(Color.WHITE); // TODO configurable via xml
-		result.setGravity(Gravity.CENTER);
-		return result;
-	}
+    public void setDate(DateTime dateTime) {
+        setDate(dateTime, true);
+    }
 
-	protected DaysGridView getCurrentDaysGridView() {
-		return mCurrentDaysGridView;
-	}
+    public OnDateClickListener getOnDateClickListener() {
+        return mOnDateClickListener;
+    }
 
-	public DateTime getDate() {
-		return mDateTime;
-	}
+    public void setOnDateClickListener(OnDateClickListener onDateClickListener) {
+        mOnDateClickListener = onDateClickListener;
+    }
 
-	public OnDateClickListener getOnDateClickListener() {
-		return mOnDateClickListener;
-	}
+    public DateTime getSelectedDate() {
+        return mSelectedDate;
+    }
 
-	public DateTime getSelectedDate() {
-		return mSelectedDate;
-	}
+    protected void setSelectedDate(DateTime dateTime) {
+        if (isSelectable()) mSelectedDate = dateTime;
+    }
 
-	private void initialize(Context context) {
-		View.inflate(context, R.layout.view_calendar, this);
-		bindViews();
-		initizlizeWeekHeader();
-		initializeButtonsListeners();
-		mDateTime = DateTime.today(TimeZone.getDefault());
-		setDate(mDateTime, false);
-	}
+    private void initialize(Context context) {
+        View.inflate(context, R.layout.view_calendar, this);
+        bindViews();
+        initizlizeWeekHeader();
+        initializeButtonsListeners();
+        mDateTime = DateTime.today(TimeZone.getDefault());
+        setDate(mDateTime, false);
+    }
 
-	private void initializeButtonsListeners() {
-		mOnClickListener = new OnClickListenerImplementation();
-		setButtonOnClickListener(R.id.vc_button_previous_year);
-		setButtonOnClickListener(R.id.vc_button_previous_month);
-		setButtonOnClickListener(R.id.vc_button_next_month);
-		setButtonOnClickListener(R.id.vc_button_next_year);
-		setButtonOnClickListener(R.id.vc_button_pick_date);
-	}
+    private void initializeButtonsListeners() {
+        mOnClickListener = new OnClickListenerImplementation();
+        setButtonOnClickListener(R.id.vc_button_previous_year);
+        setButtonOnClickListener(R.id.vc_button_previous_month);
+        setButtonOnClickListener(R.id.vc_button_next_month);
+        setButtonOnClickListener(R.id.vc_button_next_year);
+        setButtonOnClickListener(R.id.vc_button_pick_date);
+    }
 
-	private void initizlizeWeekHeader() {
-		int columns = DaysGridView.getWeekHeaderColumnCount();
-		mWeekHeaderLinearLayout.setWeightSum(columns);
-		mWeekHeaderTextViews = new TextView[columns];
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1);
-		params.gravity = Gravity.CENTER;
-		for (int i = 0; i < mWeekHeaderTextViews.length; i++) {
-			mWeekHeaderTextViews[i] = createWeekHeaderTextView();
-			mWeekHeaderLinearLayout.addView(mWeekHeaderTextViews[i], params);
-		}
-	}
+    private void initizlizeWeekHeader() {
+        int columns = DaysGridView.getWeekHeaderColumnCount();
+        mWeekHeaderLinearLayout.setWeightSum(columns);
+        mWeekHeaderTextViews = new TextView[columns];
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1);
+        params.gravity = Gravity.CENTER;
+        for (int i = 0; i < mWeekHeaderTextViews.length; i++) {
+            mWeekHeaderTextViews[i] = createWeekHeaderTextView();
+            mWeekHeaderLinearLayout.addView(mWeekHeaderTextViews[i], params);
+        }
+    }
 
-	public boolean isSelectable() {
-		return mSelectable;
-	}
+    public boolean isSelectable() {
+        return mSelectable;
+    }
 
-	public void onDateClick(DateTime date) {
-		setSelectedDate(date);
-		mDateTime = date;
-		if (getOnDateClickListener() == null) return;
-		getOnDateClickListener().onDateClick(date);
-	}
+    public void setSelectable(boolean selectable) {
+        mSelectable = selectable;
+    }
 
-	@Override
-	public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-		/* +1 because The month that was set (0-11) for compatibility with Calendar. */
-		setDate(DateTime.forDateOnly(year, monthOfYear + 1, dayOfMonth));
-	}
+    public void onDateClick(DateTime date) {
+        setSelectedDate(date);
+        mDateTime = date;
+        if (getOnDateClickListener() == null) return;
+        getOnDateClickListener().onDateClick(date);
+    }
 
-	@Override
-	public boolean onInterceptTouchEvent(MotionEvent ev) {
-		return mOnSwipeTouchListener.onTouch(this, ev);
-	}
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        /* +1 because The month that was set (0-11) for compatibility with Calendar. */
+        setDate(DateTime.forDateOnly(year, monthOfYear + 1, dayOfMonth));
+    }
 
-	private void prepareAnimation(DateTime dateTime) {
-		if (dateTime.lt(mDateTime)) {
-			mSwitcherView.setInAnimation(getContext(), R.anim.slide_in_left);
-			mSwitcherView.setOutAnimation(getContext(), R.anim.slide_out_right);
-		} else {
-			mSwitcherView.setOutAnimation(getContext(), R.anim.slide_out_left);
-			mSwitcherView.setInAnimation(getContext(), R.anim.slide_in_right);
-		}
-	}
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return mOnSwipeTouchListener.onTouch(this, ev);
+    }
 
-	private void setButtonOnClickListener(int resId) {
-		View view = findViewById(resId);
-		view.setOnClickListener(mOnClickListener);
-	}
+    private void prepareAnimation(DateTime dateTime) {
+        if (dateTime.lt(mDateTime)) {
+            mSwitcherView.setInAnimation(getContext(), R.anim.slide_in_left);
+            mSwitcherView.setOutAnimation(getContext(), R.anim.slide_out_right);
+        } else {
+            mSwitcherView.setOutAnimation(getContext(), R.anim.slide_out_left);
+            mSwitcherView.setInAnimation(getContext(), R.anim.slide_in_right);
+        }
+    }
 
-	public void setDate(DateTime dateTime) {
-		setDate(dateTime, true);
-	}
+    private void setButtonOnClickListener(int resId) {
+        View view = findViewById(resId);
+        view.setOnClickListener(mOnClickListener);
+    }
 
-	public void setDate(DateTime dateTime, boolean animation) {
-		setSelectedDate(dateTime);
-		boolean showAnimation = false;
-		if (animation && (mDateTime.getMonth().intValue() != dateTime.getMonth().intValue() || mDateTime.getYear().intValue() != dateTime.getYear().intValue())) {
-			prepareAnimation(dateTime);
-			mCurrentDaysGridView = (DaysGridView) mSwitcherView.getNextView();
-			showAnimation = true;
-		}
-		String title = dateTime.format("MMMM YYYY", Locale.getDefault()).toUpperCase(Locale.getDefault());
-		mTitleView.setText(title);
-		mDateTime = dateTime;
-		mCurrentDaysGridView.setDate(dateTime);
-		if (showAnimation) {
-			mSwitcherView.showNext();
-		}
-		updateWeekHeader();
-	}
+    public void setDate(DateTime dateTime, boolean animation) {
+        setSelectedDate(dateTime);
+        boolean showAnimation = false;
+        if (animation && (mDateTime.getMonth().intValue() != dateTime.getMonth().intValue() || mDateTime.getYear().intValue() != dateTime.getYear().intValue())) {
+            prepareAnimation(dateTime);
+            mCurrentDaysGridView = (DaysGridView) mSwitcherView.getNextView();
+            showAnimation = true;
+        }
+        String title = dateTime.format("MMMM YYYY", Locale.getDefault()).toUpperCase(Locale.getDefault());
+        mTitleView.setText(title);
+        mDateTime = dateTime;
+        mCurrentDaysGridView.setDate(dateTime);
+        if (showAnimation) {
+            mSwitcherView.showNext();
+        }
+        updateWeekHeader();
+    }
 
-	public void setOnDateClickListener(OnDateClickListener onDateClickListener) {
-		mOnDateClickListener = onDateClickListener;
-	}
+    public void setProgressVisible(boolean visible) {
+        if (!showProgress) return;
+        if (visible) {
+            mProgressView.setVisibility(View.VISIBLE);
+        } else {
+            mProgressView.setVisibility(View.GONE);
+        }
+    }
 
-	public void setProgressVisible(boolean visible) {
-		if (visible) {
-			mProgressView.setVisibility(View.VISIBLE);
-		} else {
-			mProgressView.setVisibility(View.GONE);
-		}
-	}
+    public void updateCells() {
+        getCurrentDaysGridView().updateDaysCells();
+    }
 
-	public void setSelectable(boolean selectable) {
-		mSelectable = selectable;
-	}
+    private void updateWeekHeader() {
+        for (int i = 0; i < mWeekHeaderTextViews.length; i++) {
+            mWeekHeaderTextViews[i].setText(mCurrentDaysGridView.getWeekHeaderColumnTitle(i));
+        }
+    }
 
-	protected void setSelectedDate(DateTime dateTime) {
-		if (isSelectable()) mSelectedDate = dateTime;
-	}
+    public void setShowProgress(boolean showProgress) {
+        this.showProgress = showProgress;
+    }
 
-	public void updateCells() {
-		getCurrentDaysGridView().updateDaysCells();
-	}
+    public interface OnDateClickListener {
+        void onDateClick(DateTime date);
+    }
 
-	private void updateWeekHeader() {
-		for (int i = 0; i < mWeekHeaderTextViews.length; i++) {
-			mWeekHeaderTextViews[i].setText(mCurrentDaysGridView.getWeekHeaderColumnTitle(i));
-		}
-	}
-
+    private final class OnClickListenerImplementation implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            if (id == R.id.vc_button_next_month) {
+                addMonth(1);
+            } else if (id == R.id.vc_button_previous_month) {
+                addMonth(-1);
+            } else if (id == R.id.vc_button_next_year) {
+                addMonth(12);
+            } else if (id == R.id.vc_button_previous_year) {
+                addMonth(-12);
+            } else if (id == R.id.vc_button_pick_date) {
+                new DatePickerDialog(getContext(), CalendarView.this, mDateTime.getYear(), mDateTime.getMonth() - 1, mDateTime.getDay()).show();
+            }
+        }
+    }
 }
